77b7ec179cbc75e23b670a1d201b94d4f32d49c4
Andrey Grachev <andreysgra@gmail.com>
2026-04-07 18:24:57 +0300

    Удаляет тестовые данные.

--- js/utils.js
+++ js/utils.js
@@ -1,14 +1,4 @@
-export const getRandomPositiveInteger = (a = 0, b = 1) => {
-
-  const min = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const max = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
 export const checkStringLength = (string, length) => string.length <= length;
-
-export const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
 export const addEscapeEvent = (evt, action) => {
   if (evt.key === 'Escape') {
